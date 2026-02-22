a8a360ea187ab5e23e6b37454d447b9e7d04ece5
Rabbi <fazle.rabbi@bracits.com>
2026-02-22 20:48:04 +0600

    update

---
+++ src/main/java/org/ostad/ostad_assignment_25/OstadAssignment25Application.java
@@ -0,0 +1,13 @@
+package org.ostad.ostad_assignment_25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OstadAssignment25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OstadAssignment25Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/ostad/ostad_assignment_25/OstadAssignment25ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ostad.ostad_assignment_25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OstadAssignment25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
